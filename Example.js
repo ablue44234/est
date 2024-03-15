0186a5f17831c4a205058150ef6731619670d485
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-03-15 03:05:27 +0100

    Create Example.js

---
+++ Example.js
@@ -0,0 +1,29 @@
+// Example of working with numbers in JavaScript
+
+// Creating variables to store numbers
+var number1 = 10;
+var number2 = 5;
+
+// Addition
+var sum = number1 + number2;
+console.log("Sum of numbers: " + sum); // Outputting the result to the console
+
+// Subtraction
+var difference = number1 - number2;
+console.log("Difference of numbers: " + difference); // Outputting the result to the console
+
+// Multiplication
+var product = number1 * number2;
+console.log("Product of numbers: " + product); // Outputting the result to the console
+
+// Division
+var quotient = number1 / number2;
+console.log("Quotient of numbers: " + quotient); // Outputting the result to the console
+
+// Remainder (Modulus)
+var remainder = number1 % number2;
+console.log("Remainder of dividing numbers: " + remainder); // Outputting the result to the console
+
+// Exponentiation
+var power = Math.pow(number1, number2);
+console.log("Number " + number1 + " raised to the power of " + number2 + " is: " + power); // Outputting the result to the console
