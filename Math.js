da59c1711fa688f074253602dc77c00e90cabc43
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-04-03 04:50:48 +0200

    Create Math.js

---
+++ Math.js
@@ -0,0 +1,7 @@
+public class MathRoundExample {
+    public static void main(String[] args) {
+        double num = 3.7;
+        double rounded = Math.round(num);
+        System.out.println("Rounded value: " + rounded);
+    }
+}
