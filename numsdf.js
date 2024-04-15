93bf54e859cf4c08d7d0f5a26b4f6a0314d6e51a
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-04-15 05:00:37 +0200

    Create numsdf.js

---
+++ numsdf.js
@@ -0,0 +1,9 @@
+public class ForEachExample {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println("Array elements:");
+        for (int num : array) {
+            System.out.println(num);
+        }
+    }
+}
