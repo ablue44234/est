9583769e7e63cd4e4ed87d25cba9ddae8af24372
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-04-16 05:41:16 +0200

    Create Choice.js

---
+++ Choice.js
@@ -0,0 +1,18 @@
+public class SwitchExample {
+    public static void main(String[] args) {
+        int choice = 2;
+        switch (choice) {
+            case 1:
+                System.out.println("Choice is 1");
+                break;
+            case 2:
+                System.out.println("Choice is 2");
+                break;
+            case 3:
+                System.out.println("Choice is 3");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+}
