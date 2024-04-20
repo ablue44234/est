97bdfe5cab9a5eca350e05b74a0047df868dd146
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-04-20 04:45:57 +0200

    Create indexsfsd.js

---
+++ indexsfsd.js
@@ -0,0 +1,7 @@
+public class CharAtExample {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        char ch = str.charAt(7);
+        System.out.println("Character at index 7: " + ch);
+    }
+}
