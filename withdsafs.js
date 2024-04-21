83c3c6190c143ecf62f0c026a93eafacdf9450ed
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-04-21 04:56:30 +0200

    Create withdsafs.js

---
+++ withdsafs.js
@@ -0,0 +1,10 @@
+public class StringStartsWith {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        if (str.startsWith("Hello")) {
+            System.out.println("String starts with 'Hello'.");
+        } else {
+            System.out.println("String does not start with 'Hello'.");
+        }
+    }
+}
