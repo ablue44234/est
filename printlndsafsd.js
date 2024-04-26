438c0db078e0ca69ae2048eec424396e2b2dc8c6
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-04-26 03:36:26 +0200

    Create printlndsafsd.js

---
+++ printlndsafsd.js
@@ -0,0 +1,7 @@
+public class TernaryOperatorExample {
+    public static void main(String[] args) {
+        int a = 10, b = 20, max;
+        max = (a > b) ? a : b;
+        System.out.println("Maximum of " + a + " and " + b + " is " + max);
+    }
+}
