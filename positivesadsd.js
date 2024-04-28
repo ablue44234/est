08b4c1da94b58fc963695cae7a9429caeee3e09c
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-04-28 04:17:38 +0200

    Create positivesadsd.js

---
+++ positivesadsd.js
@@ -0,0 +1,12 @@
+public class ConditionalExample {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 0) {
+            System.out.println("Number is positive.");
+        } else if (x < 0) {
+            System.out.println("Number is negative.");
+        } else {
+            System.out.println("Number is zero.");
+        }
+    }
+}
