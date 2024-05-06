6f43e4936661da3297125cfbcfd30d7fd80af7b5
ablue44234 <162697198+ablue44234@users.noreply.github.com>
2024-05-06 04:51:00 +0200

    Create Stringqw23r23.js

---
+++ Stringqw23r23.js
@@ -0,0 +1,10 @@
+public class PowerCalculator {
+    public static void main(String[] args) {
+        int base = 2, exponent = 3;
+        long result = 1;
+        for (int i = 0; i < exponent; ++i) {
+            result *= base;
+        }
+        System.out.println("2 to the power of 3 = " + result);
+    }
+}
